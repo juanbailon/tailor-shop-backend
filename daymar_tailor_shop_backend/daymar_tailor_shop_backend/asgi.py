f731f086f1540b56199ccc8dfa85cc1dcc34047a
juanBailon <juanojo2001.5@gmail.com>
2024-04-20 18:47:09 -0500

    firts commit, configuring inital settings

---
+++ daymar_tailor_shop_backend/daymar_tailor_shop_backend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for daymar_tailor_shop_backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'daymar_tailor_shop_backend.settings')
+
+application = get_asgi_application()
