9ea2f6d7c30117a2445bc6721a9a2cb9a09dae18
juanBailon <juanojo2001.5@gmail.com>
2024-04-21 16:22:17 -0500

    fixing typo in CustomUser model

--- daymar_tailor_shop_backend/users/models.py
+++ daymar_tailor_shop_backend/users/models.py
@@ -43,7 +43,7 @@ class CustomUser(AbstractUser, PermissionsMixin):
     
     USERNAME_FIELD = "email"
     #password is always required by default
-    REQUIRED_FIELDS = ['email']
+    REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
 
